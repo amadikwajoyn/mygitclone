3a4baba069a3364dcec2b7f13e00f3f955266db2
amadikwajoyn <amadikwajoyn@gmail.com>
2022-05-03 00:09:03 +0100

    remove strict mode

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ import { Provider } from "react-redux";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { store } from "./store";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import './App.css';
+import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 reportWebVitals();
